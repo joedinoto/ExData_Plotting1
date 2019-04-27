7b1e89ea6417c4f4ef9341b24e0de2f7602bba8e
Joe DiNoto <jdinoto2@jh.edu>
2019-04-27 10:16:15 -0400

    plot 1 and 2 done with pngs

--- project.R
+++ project.R
@@ -11,6 +11,10 @@ household <- fread("household_power_consumption.txt")%>%
 household <- mutate(household, DateTime = paste(Date,Time, sep=" "))
 household$DateTime <- dmy_hms(household$DateTime)
 
+# Filter the dates to 2007-02-01 and 2007-02-02 only.
+# https://www.earthdatascience.org/courses/earth-analytics/time-series-data/subset-time-series-data-in-r/
+household <- filter(household,DateTime< as.Date("2007-02-03 00:00:00") & DateTime > as.Date("2007-02-01 00:00:00"))
+
 #  assign column variable types
 household$Date <- dmy(household$Date)
 household$Time <- hms(household$Time)
@@ -21,17 +25,19 @@ household$Global_intensity <- as.numeric(household$Global_intensity)
 household$Sub_metering_1 <- as.numeric(household$Sub_metering_2)
 household$Sub_metering_2 <- as.numeric(household$Sub_metering_2)
 
-# Filter the dates to 2007-02-01 and 2007-02-02 only.
-# https://www.earthdatascience.org/courses/earth-analytics/time-series-data/subset-time-series-data-in-r/
-household_sub <- filter(household,DateTime< as.Date("2007-02-03 00:00:00") & DateTime > as.Date("2007-02-01 00:00:00"))
+# Plot1 -- Histogram
+with(household, hist(Global_active_power, 
+                     col="red",
+                     xlab="Global Active Power (killowatts)",
+                     main="Global Active Power")
+     )
+# Making a PNG
+# Warning - plot in the output copy may not look exactly like plot on screen!
+
+dev.copy(png,file="Plot1.png") # open graphics device
+dev.off() #close graphics device
+
+# Other notes
 
 #http://rfunction.com/archives/1912
 #strptime() and as.Date()
-
-# dplyr to mutate 
-household_sub %>%
-  mutate(DayAndTime = paste(Date,Time,sep=" "))
-
-# The Histogram
-with(household_sub, hist(Global_active_power, col="red",xlab="Global Active Power (killowatts)"))
-
